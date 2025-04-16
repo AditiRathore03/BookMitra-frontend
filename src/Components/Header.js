8c81d6df19a1c6ce1ba1b4a6316a2f17b4824b41
Adarsh Singh <adarsh0727@gmail.com>
2025-04-17 01:10:56 +0530

    made changes, works fine now

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,7 +20,7 @@ function Header() {
       </div>
 
       <div className='nav-right'>
-        <input className='search-input' type='text' placeholder='Search a Book' />
+        {/* <input className='search-input' type='text' placeholder='Search a Book' /> */}
         <ul className={menutoggle ? "nav-options active" : "nav-options"}>
           <li className="option" onClick={closeMenu}>
             <Link to='/'>Home</Link>
